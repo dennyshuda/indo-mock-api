d8a0c9826d6c8f5d605013fa4d4a79962a5b52d0
Denny Setyawan Huda <denycam9@gmail.com>
2026-02-20 05:45:01 +0700

    feat: route to generate nik and return province

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,5 +1,6 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import namesData from "../../data/names.json";
+import regions from "../../data/region.json";
 import { generateFakeNIK, getRandomItem } from "../utils";
 
 const v1 = new Hono();
@@ -16,6 +17,26 @@ v1.get("/user/random", (c) => {
 			full_name: `${firstName} ${lastName}`,
 			gender: parseInt(nik.substring(6, 8)) > 40 ? "Perempuan" : "Laki-laki",
 			email: `${firstName.toLowerCase()}@example.id`,
+		},
+	});
+});
+
+v1.get("/region/provinces", (c) => {
+	return c.json({
+		status: "success",
+		data: regions,
+	});
+});
+
+v1.get("/generate/nik", (c) => {
+	const provinceId = c.req.query("province");
+	const nik = generateFakeNIK(provinceId || "32");
+
+	return c.json({
+		status: "success",
+		data: {
+			nik: nik,
+			note: "Ini adalah NIK buatan untuk keperluan testing.",
 		},
 	});
 });
